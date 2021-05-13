777417e9ad9762fa098f82b5f849476d3eed7f2f
Phat Giang <phatgiang1995@gmail.com>
2021-05-13 15:27:27 -0700

    Finished 1 and 2

--- test/phase-2-spec.js
+++ test/phase-2-spec.js
@@ -9,24 +9,24 @@ const { mirrorArray, hiddenCounter, myMap, avgValue } = require('../problems/pha
 describe('mirrorArray()', function () {
   it('should return a single array where the first half is the orginal array and the second half is a mirror of the first half', function () {
     //Arrange
-
+    let expected = [1,2,3,3,2,1]
     //Act
-    
+    let actual = mirrorArray([1, 2, 3])
     //Assert
-    expect.fail('Remove this expect.fail and replace it with your test');
+    expect(actual).to.eql(expected)
   })
 })
 
 //2. hiddenCounter
 describe('hiddenCounter()', function () {
-  
+
   it('should return a function that will increment the counter when invoked', function () {
     //Arrange
-
+    let expected = 1;
     //Act
-
+    let actual = hiddenCounter()()
     //Assert
-    expect.fail('Remove this expect.fail and replace it with your test');
+    expect(actual).to.eql(expected)
   })
 })
 
@@ -34,16 +34,16 @@ describe('hiddenCounter()', function () {
 describe('myMap', function () {
  it("should function like the built in Array#map", function () {
     //Arrange
-   
+
     //Act
-   
+
     //Assert
    expect.fail('Remove this expect.fail and replace it with your test');
   });
 
   it("should not call the built in Array#map", function () {
      //Arrange
-    
+
     //Act
 
     //Assert
@@ -55,7 +55,7 @@ describe('myMap', function () {
 describe('avgValue', function () {
   it('should return the average of an array of numbers', function () {
      //Arrange
-    
+
     //Act
 
     //Assert
